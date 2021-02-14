f69777b6a742ce942c03af220ce42b3054accde9
Tirth Trivedi <tirtht2@gmail.com>
2021-02-14 12:34:47 +0530

    New home page design

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -15,91 +15,12 @@ import { Observable } from "rxjs";
   styleUrls: ["home.page.scss"],
 })
 export class HomePage {
-  task: AngularFireUploadTask;
-  percentage$: Observable<any>;
-  snapshot$: Observable<any>;
-  uploadedFileUrl: string;
-  images: Observable<any>;
+  slideOpts = {
+    initialSlide: 0,
+    speed: 200,
+    autoplay: true,
+    loop: true,
+  };
 
-  fileName: string;
-  fileSize: number;
-
-  isUploading: boolean;
-  isUploaded: boolean;
-
-  constructor(
-    private auth: AuthService,
-    private storage: AngularFireStorage,
-    private db: AngularFirestore,
-    private story: StoryService
-  ) {
-    this.isUploading = false;
-    this.isUploaded = false;
-  }
-
-  uploadFile(event: File) {
-    console.log("here");
-    const file = event["0"];
-    console.log(file);
-    if (file.type.split("/")[0] !== "image") {
-      console.log("Not a valid file type");
-      return;
-    }
-
-    this.isUploading = true;
-    this.isUploaded = false;
-
-    this.fileName = file.name;
-
-    const path = `storyImages/${new Date().getTime()}_${this.fileName}`;
-
-    const fileRef = this.storage.ref(path);
-
-    this.task = this.storage.upload(path, file);
-
-    this.percentage$ = this.task.percentageChanges();
-    console.log("Here before snapshot changes");
-    this.snapshot$ = this.task.snapshotChanges().pipe(
-      tap(console.log),
-      finalize(async () => {
-        console.log("Finalizing");
-        this.uploadedFileUrl = await fileRef.getDownloadURL().toPromise();
-        console.log(this.uploadedFileUrl);
-        this.addStory({
-          title: "Yes",
-          description: "This works",
-          imageUrl: this.uploadedFileUrl,
-          imagePath: path,
-        });
-      })
-    );
-  }
-
-  addStory(story) {
-    this.db
-      .collection("Story")
-      .add(story)
-      .then(() => {
-        console.log("story added successfully");
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
-
-  openFileSelector() {
-    document.getElementById("uploadBtn").click();
-  }
-
-  signout() {
-    this.auth.signout();
-  }
-
-  markFavourite() {
-    this.story.markFavourite("abc");
-  }
-
-  removeFavourite() {
-    this.story.removeFromFavourite("abc");
-  }
+  constructor() {}
 }
